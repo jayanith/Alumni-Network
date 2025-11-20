76d2dcfd8231e709bf414d772473c61ba0a044b0
jayanith <jayanithchowdhary@gmail.com>
2025-11-20 14:15:50 +0530

    Added static files config for Railway

--- ocmsproject/settings.py
+++ ocmsproject/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 
 from django.conf.global_settings import DATABASES
 
+import os
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -54,6 +58,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'ocmsproject.urls'
@@ -141,9 +147,12 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
