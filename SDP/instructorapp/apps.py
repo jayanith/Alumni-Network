e45506bd1b7726ae003fefecc10e2d6c176128a0
jayanith <jayanithchowdhary@gmail.com>
2025-11-20 14:01:20 +0530

    updated

--- SDP/instructorapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class InstructorappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'instructorapp'
--- SDP/instructorapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("instructorlogincheck", views.instructorlogincheck, name="instructorlogincheck"),
-    path("instructorhome", views.instructorhome, name="instructorhome"),
-    path("instructorlogout", views.instructorlogout, name="instructorlogout"),
-    path("instructorcourses", views.instructorcourses, name="instructorcourses"),
-]
